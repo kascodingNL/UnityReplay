f68e95b7ce17514f9bbc8fb70c8b313e2db94f16
kascodingNL <kas@kasnetwork.nl>
2021-02-27 13:25:08 +0100

    Added StorableTick, finished StorableObject

--- Assets/Scripts/StorableObject.cs
+++ Assets/Scripts/StorableObject.cs
@@ -1,12 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Newtonsoft.Json;
 
-public struct StorableObject
+namespace ReplaySystem
 {
-    public int ObjectId;
+    [JsonObject]
+    public class StorableObject
+    {
+        [JsonProperty]
+        public int ObjectId;
 
-    public float X, Y, Z;
+        [JsonProperty]
+        public float X;
+        public float Y;
+        [JsonProperty]
+        public float Z;
 
-    
+        [JsonConstructor]
+        public StorableObject(int ObjectId, float x, float y, float z)
+        {
+            this.ObjectId = ObjectId;
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+    }
 }
---
+++ Assets/Scripts/StorableTick.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplaySystem
+{
+    [JsonObject]
+    public class StorableTick
+    {
+        [JsonProperty]
+        public Dictionary<int, StorableObject> StorableObjectsData = new Dictionary<int, StorableObject>();
+
+        [JsonConstructor]
+        public StorableTick(Dictionary<int, StorableObject> data)
+        {
+            this.StorableObjectsData = data;
+        }
+    }
+}
